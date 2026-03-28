1611b1446ae1a1c899f453846007afaab460c0eb
MichaelVyshnovsky <michaelvyshnovsky@gmail.com>
2026-03-28 18:36:01 -0400

    Reformatting and explaintions

--- ratio_bot.py
+++ ratio_bot.py
@@ -5,6 +5,11 @@ import json
 import os
 import string
 
+#Establish bot Id so bot doesnt respond to itself
+#Change Bot Id to own bot
+BotId = 177504009748217856
+
+#Establish the Client for the Discord Bot
 client = discord.Client(intents=discord.Intents.all())
 # bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
     
@@ -12,9 +17,14 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_ready():
     print(f'{client.user} is now ready')
 
+#Ratio Code
 @client.event
+#Checks at a sent message
 async def on_message(message):
-    
+
+    Non_Ratio_Channel = {"vent-advice-channel"}
+
+    #retrives previous leaderboard info
     server_name = message.guild.name
     filename = f'{server_name}.json'
 
@@ -27,27 +37,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    #Retrives User and Channel info
     username = str(message.author)
     user_message = str(message.content)
     channel = str(message.channel)
 
-    send_message = False
-    shiny_message = False
-
-    rnd = random.randint(0,75)
-    rndShiny = random.randint(0,4080)
-    
-
-    if rndShiny == 4000:
-        shiny_message = True
-
-    if shiny_message == True:
-        await message.channel.send(f"{message.author.mention} I actually kinda like this one")
-
-    if rnd == 10:
-        send_message = True
-
-    if message.author.id != 177504009748217856 and send_message == True:
+    # Checks message for Ratio base on 
+    # 1.Random odds for Ratio
+    # 2.If the author is not the Bot
+    # 3.If the message is in the non allowed Channels
+    #
+    # If all pass the bot will Ratio
+    if random.randint(0,75) == 10 and message.author.id != BotId and not(channel in Non_Ratio_Channel):
         await message.channel.send(f"{message.author.mention} Ratio")
         
         if bool(leaderboard.get(f'<@{message.author.id}>')) is True:
@@ -59,36 +60,38 @@ async def on_message(message):
 
         with open(filename, 'w') as f:
             json.dump(leaderboard, f)
+
+    elif random.randint(0,4080) == 4000 and message.author.id != BotId and not(channel in Non_Ratio_Channel):
+        await message.channel.send(f"{message.author.mention} I actually kinda like this one")
     
-    if str(message.content) == '!ratio':
-        if bool(leaderboard) == True:
-            num = 0
-            embed = discord.Embed(title=f'Leaderboard for {message.guild.name}', description='the top 10 yuh')
-            embed.set_thumbnail(url=message.guild.icon)
-            for key, value in leaderboard.items():
-                user_id = key.translate(str.maketrans('', '', string.punctuation))
-                user = client.get_user(int(user_id))
-                
-                # If not in cache, try to fetch from Discord API
-                if user is None:
-                    try:
-                        user = await client.fetch_user(int(user_id))
-                    except:
-                        user_name = "Unknown User"
-                    else:
-                        user_name = user.name
+    # Code for viewing the leaderboard
+    if str(message.content) == '!ratio' and bool(leaderboard) == True:
+        num = 0
+        embed = discord.Embed(title=f'Leaderboard for {message.guild.name}', description='the top 10 yuh')
+        embed.set_thumbnail(url=message.guild.icon)
+        for key, value in leaderboard.items():
+            user_id = key.translate(str.maketrans('', '', string.punctuation))
+            user = client.get_user(int(user_id))
+
+            # If not in cache, try to fetch from Discord API
+            if user is None:
+                try:
+                    user = await client.fetch_user(int(user_id))
+                except:
+                    user_name = "Unknown User"
                 else:
                     user_name = user.name
+            else:
+                user_name = user.name
+        
+            if num <= 10:
+                embed.add_field(name=f'{user_name} has been ratioed:', value=f'{value} times', inline=False)
+                num += 1
+        await message.channel.send(embed=embed)
+    else:
+        await message.channel.send('No one in this server has been ratioed yet :pensive:')
             
-                if num <= 10:
-                    embed.add_field(name=f'{user_name} has been ratioed:', value=f'{value} times', inline=False)
-                    num += 1
-            await message.channel.send(embed=embed)
-        else:
-            await message.channel.send('No one in this server has been ratioed yet :pensive:')
-            
-
-            
+  
 # @bot.command(name='lb')
 # async def lb(ctx, arg):
 #     server_name = ctx.guild
@@ -103,9 +106,6 @@ async def on_message(message):
 #         leaderboard[ctx.message.author] += 1
 #     except KeyError:
 #         leaderboard[ctx.message.author] = 1
-    
-    
-
 
 client.run('MTA1MTc3NTYyNTg0MjY3MTcwNg.GBbrwb.KZdVoCzAEZdLKYcEkrFjqy1LMEbuTBYhqTe59k')
 # ^ Security token
